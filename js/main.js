b18d4dde698b87ae452c5f9d8543c94caa4626d3
Evan Sheehan <wallace.sheehan@gmail.com>
2012-05-17 15:50:40 -0400

    Add a nivoslider and some images.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,40 @@
 
 */
 
+EPILOGIC = {
+    common: {
+        init: function() {
+            $('#slider').nivoSlider();
+        }
+    }
+}
 
+UTIL = {
+    exec: function (controller, action) {
+        var ns = EPILOGIC,
+            action = action || 'init';
 
+        if (controller !== '' && ns[controller] &&
+            typeof ns[controller][action] === 'function') {
+          
+            ns[controller][action]();
+        }
+    },
 
+    init: function () {
+        var body = document.body,
+            controller = body.getAttribute('data-controller');
+            action = body.getAttribute('data-action');
 
+        UTIL.exec('common');
+        UTIL.exec(controller);
+
+        if (action) {
+            // Don't call init twice because there's no data-action attribute
+            // on the body.
+            UTIL.exec(controller, action);
+        }
+    },
+}
+
+$(document).ready(UTIL.init);
